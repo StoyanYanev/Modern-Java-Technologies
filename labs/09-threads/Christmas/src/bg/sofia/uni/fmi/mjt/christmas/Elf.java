2ba35a8a2bcf56b54d6b3b85d4ec3fc57e79ca78
StoyanYanev <stoyanyanev98@gmail.com>
2019-12-23 09:38:22 +0200

    Refactoring run method

--- labs/09-threads/Christmas/src/bg/sofia/uni/fmi/mjt/christmas/Elf.java
+++ labs/09-threads/Christmas/src/bg/sofia/uni/fmi/mjt/christmas/Elf.java
@@ -3,8 +3,6 @@ package bg.sofia.uni.fmi.mjt.christmas;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Elf extends Thread {
-    private static final int TIMEOUT = 100;
-
     private int id;
     private final Workshop workshop;
     private AtomicInteger numberOfCraftedGifts;
@@ -19,33 +17,35 @@ public class Elf extends Thread {
      * Gets a wish from the backlog and creates the wanted gift.
      **/
     public void craftGift() throws InterruptedException {
-        synchronized (workshop) {
-            Gift currentGift = workshop.nextGift();
-            while (currentGift == null && !workshop.isChristmasTime()) {
-                workshop.wait(TIMEOUT);
-                currentGift = workshop.nextGift();
-            }
-            if (currentGift != null) {
-                Thread.sleep(currentGift.getCraftTime());
-                numberOfCraftedGifts.getAndIncrement();
-            }
-        }
+        Gift currentGift = workshop.nextGift();
+
+        Thread.sleep(currentGift.getCraftTime());
+        numberOfCraftedGifts.getAndIncrement();
     }
 
     /**
-     * Returns the total number of gifts that the given elf has crafted.
-     **/
+     * Get total gifts crafted
+     *
+     * @return the total number of gifts that the given elf has crafted.
+     */
     public int getTotalGiftsCrafted() {
         return numberOfCraftedGifts.get();
     }
 
     @Override
     public void run() {
-        while (!workshop.isChristmasTime() || workshop.getGiftsCountInBacklog() != 0) {
-            try {
-                craftGift();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        synchronized (workshop) {
+            while (!workshop.isChristmasTime()
+                    || workshop.getGiftsCountInBacklog() != 0) {
+                try {
+                    if (workshop.getGiftsCountInBacklog() != 0) {
+                        craftGift();
+                    } else {
+                        workshop.wait();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
